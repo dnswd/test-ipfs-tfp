24a04268903444d307a87d21abb46dc73aa63bd0
Dennis Walangadi <dennis.walangadi@gmail.com>
2023-06-10 14:09:01 +0000

    batch ipfs

--- ipfs.mjs
+++ ipfs.mjs
@@ -2,9 +2,7 @@ import { create as createHttpClient } from 'ipfs-http-client'
 import path from 'path'
 import { execa } from 'execa'
 
-const dlSize = '16MB'
-const id = 1
-const folder = 'ipfs'
+const nodeCount = 2
 
 const ipfs = createHttpClient({
     host: 'localhost',
@@ -47,13 +45,13 @@ async function cleanup() {
     }
 }
 
-const bitSize = {
-    '4KB': 4,
-    '16KB': 16,
-    '64KB': 64,
-    '1MB': 1 * 1024,
-    '4MB': 4 * 1024,
-    '16MB': 16 * 1024,
+const ipfsCid = {
+    '4KB': '',
+    '16KB': '',
+    '64KB': '',
+    '1MB': '',
+    '4MB': '',
+    '16MB': '',
 }
 
 async function downloadIPFS(cid) {
@@ -76,16 +74,22 @@ async function downloadIPFS(cid) {
     }
 }
 
+const downloaded = []
+
 async function main() {
-    const filename = `test1-${bitSize[dlSize]}KB`
-    const dlPath = path.join(process.cwd(), folder, filename)
-    logger(`Downloading file ${filename}`)
-    await startdump(dlPath, async () => {
-        await downloadIPFS(filename)
-    })
-    logger("Cleaning local")
-    await cleanup()
-    await wait(2)
+    for (const size in ipfsCid) {
+        if (downloaded.indexOf(size) > -1) continue
+        for (let i = 1; i <= 5; i++) {
+            logger(`Downloading file ${size}: ${ipfsCid[size]}`)
+            const dumpPath = path.join(process.cwd(), 'ipfs', size, nodeCount, i.toString())
+            await startdump(dumpPath, async () => {
+                await downloadIPFS(ipfsCid[size])
+            })
+            // logger("Cleaning local") // not stored in the disk
+            // await cleanup(path.join(process.cwd(), filename)) 
+            await wait(2)
+        }
+    }
 }
 
 main().catch(e => logger(e))
